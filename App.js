79212e4a74cd49116230cda6485509512750f56d
drissya <drissyasaseendran@gmail.com>
2020-11-19 15:57:53 +0530

    second commit

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
+import React from 'react'
+import { Provider as PaperProvider } from 'react-native-paper'
+// import AppNavigator from './Main/Navigation/HomeStack'
+import AppNavigator from './Main/Navigation/drawer'
+import { Provider as StoreProvider } from 'react-redux'
+// The <Provider /> makes the Redux store available to any nested components ..
+// that have been wrapped in the connect()/hooks -(useSelector and useDispatch ) function
+import { createStore ,applyMiddleware } from 'redux'
+import reducer from "./Main/Redux/Reducer/index"; 
+import thunk from "redux-thunk";
+import {loadState,saveState} from './Main/Redux/Store/store'
+const persistedState = loadState()
+const store = createStore(
+  reducer,
+  // vvvvvvvvvvvvvvvvvvvvvvvhttps://github.com/RasnaMusthafa/Nashdesk-app.git
+  // applyMiddleware(thunk),
+  persistedState
+);
+// A store holds the whole state tree of your application. 
+// The only way to change the state inside it is to dispatch an action on it.
+store.subscribe(() =>
+{
+  saveState(store.getState())
+})
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <StoreProvider store={store} >
+      <PaperProvider  >
+        <AppNavigator />
+      </PaperProvider>
+    </StoreProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
