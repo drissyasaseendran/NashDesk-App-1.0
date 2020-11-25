132be30049090077a4ac4f967dffea4f6cd0ec92
drissya <drissyasaseendran@gmail.com>
2020-11-25 14:04:37 +0530

    login

--- Main/Views/Login.js
+++ Main/Views/Login.js
@@ -1,16 +1,99 @@
 import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Text,Image,TouchableOpacity, View , TextInput} from 'react-native';
 import { styles } from '../Styles/styles' 
-
+import { setLogout } from "../states/login/loginAction"
+import { loginCall, isAuthenticated } from "../utils/Authenticator";
   function Login({navigation}) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const login = () => {
-        navigation.navigate("Login")
+  const dispatch = useDispatch();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [fresh, setFresh] = useState({
+    username: true,
+    password: true,
+  });
+
+  const [usrErrorCode, setUsrErrorCode] = useState(0);
+  const [pwdErrorCode, setPwdErrorCode] = useState(0);
+  const [loading, setLoading] = useState(false);
+//   const loginStatus = useSelector((state) => state.login.status);
+  const loginStatus = "success"
+  const errorRef = {
+    0: {
+      username: "",
+      password: "",
+    },
+    1: {
+      username: "* Provide a valid email format",
+      password: "* Password cannot be empty",
+    },
+    2: {
+      password: "* Invalid username or/and password provided",
+    },
+  };
+
+  const validateForm = () => {
+    if (!fresh.username) {
+      validateEmail(username) ? setUsrErrorCode(0) : setUsrErrorCode(1);
     }
-    const handleSubmit = () => {
-        // loginCall(username, password);
+    if (!fresh.password) {
+      validatePassword(password) ? setPwdErrorCode(0) : setPwdErrorCode(1);
     }
+  };
+  useEffect(() => {
+    dispatch(setLogout());
+  }, [dispatch]);
+  useEffect(() => {
+    switch (loginStatus) {
+      case "error":
+        setPwdErrorCode(0);
+        setLoading(false);
+        setNetworkErrorCode(1);
+        break;
+      case "failed":
+        setPwdErrorCode(2);
+        setLoading(false);
+        break;
+
+      case "wait":
+        setLoading(true);
+        break;
+
+      case "success":
+        if (isAuthenticated()) {
+        //   history.push("/home");
+        }
+        break;
+
+      default:
+        setLoading(false);
+        break;
+    }
+  }, [loginStatus]);
+  useEffect(() => {
+    validateForm();
+  }, [username, password]);
+
+  const handleSubmit = () => {
+    if (
+      !fresh.username &&
+      !fresh.password &&
+      usrErrorCode === 0 &&
+      pwdErrorCode === 0
+    ) {
+      setLoading(true);
+      loginCall(username, password);
+    } else {
+      if (fresh.username) {
+        setUsrErrorCode(1);
+      }
+      if (fresh.password) {
+        setPwdErrorCode(2);
+      }
+      setFresh({ username: false, password: false });
+    }
+  };
    return (
 
         <View style={styles.container}>
