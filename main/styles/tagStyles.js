9d22888419d05ad8366665b1b291b0b994a022ea
drissya <drissyasaseendran@gmail.com>
2020-12-01 17:01:12 +0530

    tags

--- main/styles/tagStyles.js
+++ main/styles/tagStyles.js
@@ -59,7 +59,7 @@ const styles = StyleSheet.create({
         fontSize:12,
         color:'#fff',
         padding:4,
-        borderRadius:10,
+        borderRadius:5,
         marginLeft:10
 
     },
--- main/views/Settings/Tags.js
+++ main/views/Settings/Tags.js
@@ -46,8 +46,58 @@ function Tags (){
 							
 						</View>
 			</TouchableOpacity>
-				
-
+			<TouchableOpacity  onLongPress={()=>pressLong()}    style={styles.tagBlock}>
+						<View style={styles.View}>
+								
+								<View style={styles.tagBlockView}>
+									<View><Icon style={styles.tagBlockIcon} name="tag-outline"/></View>
+									<View style={styles.tagcontent}>
+									<Text style={styles.tagTitle}>dsadsda</Text>
+									<Text style={styles.tagTitlesub}>Used in 50 Tickets</Text>
+									<Text style={styles.tagTitleHash}>#reused,#refund</Text>
+									</View>	
+								</View>
+							
+								<View  style={styles.tagBlockUpdation}> 
+									<View  style={styles.BtnView}> 
+										<TouchableOpacity  style={styles.btnEdit}   >
+											<Text style={styles.textColor}>Edit</Text>
+										</TouchableOpacity>
+										<TouchableOpacity   style={styles.btnDelete} >
+											<Text style={styles.textColor}>Delete</Text>
+										</TouchableOpacity>          
+									</View>
+								</View>
+							
+							
+						</View>
+			</TouchableOpacity>
+			<TouchableOpacity  onLongPress={()=>pressLong()}    style={styles.tagBlock}>
+						<View style={styles.View}>
+								
+								<View style={styles.tagBlockView}>
+									<View><Icon style={styles.tagBlockIcon} name="tag-outline"/></View>
+									<View style={styles.tagcontent}>
+									<Text style={styles.tagTitle}>dsadsda</Text>
+									<Text style={styles.tagTitlesub}>Used in 50 Tickets</Text>
+									<Text style={styles.tagTitleHash}>#reused,#refund</Text>
+									</View>	
+								</View>
+							
+								<View  style={styles.tagBlockUpdation}> 
+									<View  style={styles.BtnView}> 
+										<TouchableOpacity  style={styles.btnEdit}   >
+											<Text style={styles.textColor}>Edit</Text>
+										</TouchableOpacity>
+										<TouchableOpacity   style={styles.btnDelete} >
+											<Text style={styles.textColor}>Delete</Text>
+										</TouchableOpacity>          
+									</View>
+								</View>
+							
+							
+						</View>
+			</TouchableOpacity>
 				
 			</View>
 		</ScrollView>
