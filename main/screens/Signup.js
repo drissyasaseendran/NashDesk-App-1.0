4b7235deec6ba69e472cd2b949d0cc76c4a32aa3
drissya <drissyasaseendran@gmail.com>
2020-12-10 11:29:24 +0530

    signup

--- main/screens/Signup.js
+++ main/screens/Signup.js
@@ -12,6 +12,7 @@ import {
     StatusBar
 } from 'react-native';
 import * as Animatable from 'react-native-animatable';
+import { styles } from '../styles/signupStyles' 
 // import LinearGradient from 'react-native-linear-gradient';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Feather from 'react-native-vector-icons/Feather';
@@ -212,6 +213,11 @@ const SignInScreen = ({navigation}) => {
                         color: '#0187CA'
                     }]}>Sign In</Text>
                 </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => navigation.goBack()}>
+                <Text style={[ {
+                       
+                    }]}>Login</Text> </TouchableOpacity>
             </View>
             </ScrollView>
         </Animatable.View>
@@ -220,69 +226,3 @@ const SignInScreen = ({navigation}) => {
 };
 
 export default SignInScreen;
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1, 
-      backgroundColor: '#0187CA'
-    },
-    header: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        paddingHorizontal: 20,
-        paddingBottom: 50
-    },
-    footer: {
-        flex: Platform.OS === 'ios' ? 3 : 5,
-        backgroundColor: '#fff',
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-        paddingHorizontal: 20,
-        paddingVertical: 30
-    },
-    text_header: {
-        color: '#fff',
-        fontWeight: 'bold',
-        fontSize: 30
-    },
-    text_footer: {
-        color: '#05375a',
-        fontSize: 18
-    },
-    action: {
-        flexDirection: 'row',
-        marginTop: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: '#f2f2f2',
-        paddingBottom: 5
-    },
-    textInput: {
-        flex: 1,
-        marginTop: Platform.OS === 'ios' ? 0 : -12,
-        paddingLeft: 10,
-        color: '#05375a',
-    },
-    button: {
-        alignItems: 'center',
-        marginTop: 50
-    },
-    signIn: {
-        width: '100%',
-        height: 50,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 10
-    },
-    textSign: {
-        fontSize: 18,
-        fontWeight: 'bold'
-    },
-    textPrivate: {
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        marginTop: 20
-    },
-    color_textPrivate: {
-        color: 'grey'
-    }
-  });
