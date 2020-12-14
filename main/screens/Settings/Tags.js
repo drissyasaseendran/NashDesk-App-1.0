47e3507baaa6c508d3a62d29750f4545dc10fbf9
drissya <drissyasaseendran@gmail.com>
2020-12-14 11:46:49 +0530

    tag

--- main/screens/Settings/Tags.js
+++ main/screens/Settings/Tags.js
@@ -2,48 +2,49 @@ import React, { useEffect,useRef, useState } from "react";
 import {TextInput,ScrollView,View,TouchableOpacity,Text
 } from 'react-native';
 import { FAB } from 'react-native-paper';
+import axios from 'axios'
 import {styles} from '../../styles/tagStyles'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import TagAdd from '../../screens/Settings/TagAdd'
+import {tagApiPath} from '../../endpoints'
 function Tags (){
-	const [afterPress , setafterPress] = useState(true)
 	const [visible,setVisible] = useState(true)
-	const [tagFeild,settagFeild] = useState({
-		tag: "", })
+	const token = '2d9cc2e28cdae62ec7c6'
+	const [tag,setTag] = useState([])
 	const sheetRef = useRef(null)
 	useEffect(() => {
-	})
-	const pressLong = () =>
+		fetchTagdata()
+	},[])
+
+	const fetchTagdata = () =>
 	{
+		let data = {
+			"access_token":token,
+			"request_type":"view_count"
+		 } 
+		 axios.post(tagApiPath, data).then((respData) => {
+		   
+		  if(respData.data.status == "success")
+		  {
+			let ar = []
+			let count_view = respData.data.payload.data
+			const count = Object.entries(count_view).map(([key, value]) => {
+				
+				let data = {
+					"tag": key,
+					"value": value
+				}
+				ar.push(data)
 	
-		setafterPress(false)
+			  })
+	
+			  setTag(ar)
 		
+		  }
+	  
+		});
+	   
 	}
-    const renderContent = () => (
-		<View style={styles.EditFeild}>
-				<TextInput style = {styles.editProfile}
-					underlineColorAndroid = "transparent"
-					placeholder = "Code"
-					placeholderTextColor = "#666"
-					name="tag"
-					value={tagFeild.tag}
-					autoCapitalize = "none"
-					onChangeText={(text)=>onTagChange(text, 'tag')} 
-				/>
-				<TouchableOpacity   style={styles.btnUpdate} onPress={() => ProfileUpdate()}  >
-						<Text  style={styles.btnText} >UPDATE</Text>      
-				</TouchableOpacity>  
-		</View>
-	  );
-	  const onTagChange = (text ,stateProp) =>
-      {
-		settagFeild({ ...tagFeild, [stateProp]: text });
-        
-	 }
-	 const EditPress = () =>
-	 {
-		sheetRef.current.snapTo(1)
-	 }
 	return (
 	
 		<View>
@@ -51,76 +52,34 @@ function Tags (){
 				showsVerticalScrollIndicator={false}
 				rollEventThrottle={16}
 				>
-					<View style={styles.tagView}>
-			<TouchableOpacity  onLongPress={()=>pressLong()}    style={styles.tagBlock}>
+			<View style={styles.tagView}>
+			{tag && tag.map((tag) =>
+				{
+				return(<TouchableOpacity   style={styles.tagBlock}>
 						<View style={styles.View}>
 								<View style={styles.tagBlockView}>
 									<View><Icon style={styles.tagBlockIcon} name="tag-outline"/></View>
 									<View style={styles.tagcontent}>
-									<Text style={styles.tagTitle}>dsadsda</Text>
-									<Text style={styles.tagTitlesub}>Used in 50 Tickets</Text>
+									<Text style={styles.tagTitle}>{tag.tag} </Text>
+									<Text style={styles.tagTitlesub}>Used in {tag.value.count} Tickets</Text>
 									<Text style={styles.tagTitleHash}>#reused,#refund</Text>
 									</View>	
 								</View>
 								<View  style={styles.btn} > 
-										<TouchableOpacity  onPress={() => editGroup(group)}   >
+										<TouchableOpacity  onPress={() => editGroup(tag.value.tag_id)}   >
 										<Icon name="pencil-outline" style={styles.IconcolorEdit} fontSize='inherit' color='inherit'/>
 										</TouchableOpacity>
-										<TouchableOpacity onPress={() => deleteGroup(group.grp_id)}  >
+										<TouchableOpacity onPress={() => deleteGroup(tag.value.tag_id)}  >
 										<Icon name="trash-can-outline" style={styles.Iconcolortrash} fontSize='inherit' color='inherit'/>
 									
 										</TouchableOpacity>          
 									</View>
 						</View>
-			</TouchableOpacity>
-			<TouchableOpacity  onLongPress={()=>pressLong()}    style={styles.tagBlock}>
-						<View style={styles.View}>
-								
-								<View style={styles.tagBlockView}>
-									<View><Icon style={styles.tagBlockIcon} name="tag-outline"/></View>
-									<View style={styles.tagcontent}>
-									<Text style={styles.tagTitle}>dsadsda</Text>
-									<Text style={styles.tagTitlesub}>Used in 50 Tickets</Text>
-									<Text style={styles.tagTitleHash}>#reused,#refund</Text>
-									</View>	
-								</View>
-							
-								<View  style={styles.btn} > 
-										<TouchableOpacity  onPress={() => editGroup(group)}   >
-										<Icon name="pencil-outline" style={styles.IconcolorEdit} fontSize='inherit' color='inherit'/>
-										</TouchableOpacity>
-										<TouchableOpacity onPress={() => deleteGroup(group.grp_id)}  >
-										<Icon name="trash-can-outline" style={styles.Iconcolortrash} fontSize='inherit' color='inherit'/>
-									
-										</TouchableOpacity>          
-									</View>
-							
-							
-						</View>
-			</TouchableOpacity>
-			<TouchableOpacity  onLongPress={()=>pressLong()}    style={styles.tagBlock}>
-						<View style={styles.View}>
-								
-								<View style={styles.tagBlockView}>
-									<View><Icon style={styles.tagBlockIcon} name="tag-outline"/></View>
-									<View style={styles.tagcontent}>
-									<Text style={styles.tagTitle}>dsadsda</Text>
-									<Text style={styles.tagTitlesub}>Used in 50 Tickets</Text>
-									<Text style={styles.tagTitleHash}>#reused,#refund</Text>
-									</View>	
-								</View>
-								<View  style={styles.btn} > 
-										<TouchableOpacity  onPress={() => editGroup(group)}   >
-										<Icon name="pencil-outline" style={styles.IconcolorEdit} fontSize='inherit' color='inherit'/>
-										</TouchableOpacity>
-										<TouchableOpacity onPress={() => deleteGroup(group.grp_id)}  >
-										<Icon name="trash-can-outline" style={styles.Iconcolortrash} fontSize='inherit' color='inherit'/>
-									
-										</TouchableOpacity>          
-									</View>
-						
-						</View>
-			</TouchableOpacity>
+			</TouchableOpacity>)
+			 })
+			}
+	   
+
 			</View>
 			</ScrollView>
 			<TagAdd visible={false}/>
