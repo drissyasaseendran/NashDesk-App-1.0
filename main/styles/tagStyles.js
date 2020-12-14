8f1ca2813edae0d8f05b0805431113b5d72e5db1
drissya <drissyasaseendran@gmail.com>
2020-12-14 11:48:21 +0530

    tag

--- main/styles/tagStyles.js
+++ main/styles/tagStyles.js
@@ -51,6 +51,7 @@ const styles = StyleSheet.create({
         fontSize:16,
         color:'#666',
         paddingLeft:12,
+        // padding:5,
         // fontWeight:'bold'
     },
     tagTitlesub:
